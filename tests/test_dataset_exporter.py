dae447c508b77fc3626c2f2dbcb2cc6a8ec7ca6b
vfdev-5 <vfdev.5@gmail.com>
2018-04-13 14:56:15 +0000

    [WIP] Parallel execution

--- tests/test_dataset_exporter.py
+++ tests/test_dataset_exporter.py
@@ -10,6 +10,19 @@ from PIL import Image
 from image_dataset_viz.dataset_exporter import resize_image, DatasetExporter
 
 
+def read_img(i):
+    img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
+    return img
+
+
+def read_target(i):
+    return "label_{}".format(i)
+
+
+def img_id_fn(x):
+    return str(x)
+
+
 class TestDatasetExporter(TestCase):
 
     def setUp(self):
@@ -34,20 +47,13 @@ class TestDatasetExporter(TestCase):
             de.export_datapoint(0, "test", "test.png")
 
         with self.assertRaises(TypeError):
-            def read_img(i):
-                img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
-                return img
 
             de = DatasetExporter(read_img_fn=read_img)
             de.export_datapoint(0, 0, "test.png")
 
     def test_export_datapoint(self):
 
-        def read_img(i):
-            img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
-            return img
-
-        de = DatasetExporter(read_img_fn=read_img, img_id_fn=lambda x: str(x))
+        de = DatasetExporter(read_img_fn=read_img, img_id_fn=img_id_fn)
         with tempfile.TemporaryDirectory() as tmpdir:
             path = Path(tmpdir) / "test.png"
             de.export_datapoint(0, "test", path.as_posix())
@@ -55,11 +61,7 @@ class TestDatasetExporter(TestCase):
 
     def test_export_datapoint_target_none(self):
 
-        def read_img(i):
-            img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
-            return img
-
-        de = DatasetExporter(read_img_fn=read_img, img_id_fn=lambda x: str(x))
+        de = DatasetExporter(read_img_fn=read_img, img_id_fn=img_id_fn)
         with tempfile.TemporaryDirectory() as tmpdir:
             path = Path(tmpdir) / "test.png"
             de.export_datapoint(0, None, path.as_posix())
@@ -67,12 +69,28 @@ class TestDatasetExporter(TestCase):
 
     def test_integration(self):
 
-        def read_img(i):
-            img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
-            return img
+        n = 100
+        s = 32
+        m = 5
+        max_n_rows = 5
+        n_cols = 10
+        de = DatasetExporter(read_img_fn=read_img, read_target_fn=read_target,
+                             img_id_fn=img_id_fn,
+                             max_output_img_size=(s, s), margins=(m, m),
+                             n_cols=n_cols, max_n_rows=max_n_rows, n_workers=1)
 
-        def read_target(i):
-            return "label_{}".format(i)
+        indices = [i for i in range(n)]
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            de.export(indices, indices, output_folder=tmpdir)
+            path = Path(tmpdir)
+            out_files = list(path.glob("*.png"))
+            self.assertEqual(len(out_files), int(np.ceil(n / (n_cols * max_n_rows))))
+            for fp in out_files:
+                out_img = Image.open(fp)
+                self.assertEqual(out_img.size, ((s + m) * n_cols, (s + m) * max_n_rows))
+
+    def test_integration_mp(self):
 
         n = 100
         s = 32
@@ -80,9 +98,9 @@ class TestDatasetExporter(TestCase):
         max_n_rows = 5
         n_cols = 10
         de = DatasetExporter(read_img_fn=read_img, read_target_fn=read_target,
-                             img_id_fn=lambda x: str(x),
+                             img_id_fn=img_id_fn,
                              max_output_img_size=(s, s), margins=(m, m),
-                             n_cols=n_cols, max_n_rows=max_n_rows)
+                             n_cols=n_cols, max_n_rows=max_n_rows, n_workers=4)
 
         indices = [i for i in range(n)]
 
@@ -97,9 +115,28 @@ class TestDatasetExporter(TestCase):
 
     def test_integration_targets_none(self):
 
-        def read_img(i):
-            img = Image.new(mode='RGB', size=(64, 64), color=(i % 255, (i * 2) % 255, (i * 3) % 255))
-            return img
+        n = 100
+        s = 32
+        m = 5
+        max_n_rows = 5
+        n_cols = 10
+        de = DatasetExporter(read_img_fn=read_img,
+                             img_id_fn=img_id_fn,
+                             max_output_img_size=(s, s), margins=(m, m),
+                             n_cols=n_cols, max_n_rows=max_n_rows, n_workers=1)
+
+        indices = [i for i in range(n)]
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            de.export(indices, None, output_folder=tmpdir)
+            path = Path(tmpdir)
+            out_files = list(path.glob("*.png"))
+            self.assertEqual(len(out_files), int(np.ceil(n / (n_cols * max_n_rows))))
+            for fp in out_files:
+                out_img = Image.open(fp)
+                self.assertEqual(out_img.size, ((s + m) * n_cols, (s + m) * max_n_rows))
+
+    def test_integration_targets_none_mp(self):
 
         n = 100
         s = 32
@@ -107,9 +144,9 @@ class TestDatasetExporter(TestCase):
         max_n_rows = 5
         n_cols = 10
         de = DatasetExporter(read_img_fn=read_img,
-                             img_id_fn=lambda x: str(x),
+                             img_id_fn=img_id_fn,
                              max_output_img_size=(s, s), margins=(m, m),
-                             n_cols=n_cols, max_n_rows=max_n_rows)
+                             n_cols=n_cols, max_n_rows=max_n_rows, n_workers=4)
 
         indices = [i for i in range(n)]
 
